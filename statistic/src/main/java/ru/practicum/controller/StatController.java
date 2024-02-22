0b8c9d091761494e30d31569ed58f43470240aa8
radya100 <radya100@gmail.com>
2024-02-22 10:40:05 +0300

    feat: Фух блин Нативные запросы проще. Надо переходить на динамический SQL

--- statistic/src/main/java/ru/practicum/controller/StatController.java
+++ statistic/src/main/java/ru/practicum/controller/StatController.java
@@ -2,16 +2,20 @@ package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.model.EventIn;
 import ru.practicum.model.EventOut;
+import ru.practicum.model.EventsOut;
 import ru.practicum.service.EventService;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController
-@RequestMapping(path = "/stat")
+@RequestMapping
 @RequiredArgsConstructor
 @Validated
 public class StatController {
@@ -19,18 +23,23 @@ public class StatController {
     @Autowired
     private final EventService eventService;
 
-    @GetMapping("/hello")
-    public String getHello() {
-        return "Hello!";
-    }
-
-    @PostMapping
+    @PostMapping(path = "/hit")
     public EventOut save(@Valid @RequestBody EventIn eventIn) {
         return eventService.save(eventIn);
     }
 
-    @GetMapping("/{id}")
-    public EventOut getById(@PathVariable("id") long id) {
-        return eventService.getById(id);
+    @GetMapping(path = "/stats")
+    public List<EventsOut> getEvents(
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+            @RequestParam(required = false) String uris,
+            @RequestParam(defaultValue = "false") Boolean unique) {
+
+        List<String> urisList = new ArrayList<>();
+        if (uris != null) {
+            urisList.addAll(Arrays.asList(uris.split(",")));
+        }
+
+        return eventService.getEvents(start, end, urisList, unique);
     }
 }
--- statistic/src/main/java/ru/practicum/service/EventService.java
+++ statistic/src/main/java/ru/practicum/service/EventService.java
@@ -1,14 +1,23 @@
 package ru.practicum.service;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.practicum.error.NotFoundException;
-import ru.practicum.model.EventIn;
-import ru.practicum.model.EventMapper;
-import ru.practicum.model.EventOut;
+import ru.practicum.model.*;
+import ru.practicum.model.QEvent;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +25,9 @@ public class EventService {
 
     @Autowired
     private final EventRepository eventRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Transactional
     public EventOut save(EventIn eventIn) {
@@ -25,5 +37,37 @@ public class EventService {
     public EventOut getById(long id) {
         return EventMapper.toEventOut(eventRepository.findById(id).orElseThrow(() ->
                 new NotFoundException("Событие с идентификатором " + id + " не найдено")));
+    }
+
+    public List<EventsOut> getEvents(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+
+        List<EventsOut> returnedEvents = new ArrayList<>();
+/*        BooleanExpression byStart = QEvent.event.dt.after(LocalDateTime.from(start));
+        BooleanExpression byEnd = QEvent.event.dt.before(LocalDateTime.from(end));
+        BooleanExpression byUris = QEvent.event.uri.in(uris);
+        Iterable<Event> events = eventRepository.findAll(byStart.and(byEnd).and(byUris));*/
+
+        QEvent event = QEvent.event;
+        StringExpression appExpression = event.app;
+        StringExpression uriExpression = event.uri;
+        NumberExpression<Long> hitsExpression = unique ? event.ip.countDistinct() : event.id.count();
+
+
+        List<Tuple> tuples = new JPAQueryFactory(entityManager)
+                .select(appExpression, uriExpression, hitsExpression)
+                .from(event)
+                .where(
+                        event.dt.after(start),
+                        event.dt.before(end),
+                        event.uri.in(uris))
+                .groupBy(appExpression, uriExpression)
+                .orderBy(hitsExpression.desc())
+                .fetch();
+        for (Tuple row : tuples) {
+            returnedEvents.add(new EventsOut(row.toArray()[0].toString(),
+                    row.toArray()[1].toString(),
+                    row.toArray()[2].toString()));
+        }
+        return returnedEvents;
     }
 }
